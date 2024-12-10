c22a09ffb195218a3e38620e0a6acec845064b21
kali_muon <20001044+kali-muon@users.noreply.github.com>
2024-12-09 22:38:46 -0600

    stub out Collection and Stack

---
+++ Collection.cs
@@ -0,0 +1,16 @@
+using System.Runtime.CompilerServices;
+
+namespace blackjack;
+
+class Collection
+{
+    List<Card> cards;
+
+    public Collection(List<Card> cards)
+    {
+        this.cards = cards;
+    }
+}
+
+
+
---
+++ Stack.cs
@@ -0,0 +1,21 @@
+namespace blackjack;
+
+// describes a stack of cards, arranged like a deck where you can draw from them.
+// i'm not calling it a deck, as the word "deck" refers to a set of 52 specific cards.
+public class Stack
+{
+    // Shuffles the collection of cards
+    // needs to be able to throw an error if no cards are remaining
+    public void Shuffle()
+    {
+        
+    }
+    
+    // draws a card from the top of a collection. should i make this part of a stack of cards instead?
+    // also, this needs to be able to throw an error if there are no cards remaining
+    public Card Draw()
+    {
+        return null;
+    }
+    
+}
